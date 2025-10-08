274a62816c28e5340f50911d6c22f8ed6c1a6cae
Francy <tresoldi.francesco@studenti.salesianisesto.it>
2025-10-08 13:41:26 +0200

    Update primo.php

--- WebServer/Teoria/EsercitazionePHP/primo.php
+++ WebServer/Teoria/EsercitazionePHP/primo.php
@@ -4,8 +4,16 @@
 	$l = "<h1 style='color: red;'>Hello $nome</h1>";
 	echo($l);
 
-// " = In PHP, le virgolette doppie permettono l'interpolazione di variabili e riconoscono le sequenze di escape.
-// ' = Le virgolette singole (') trattano il contenuto come testo letterale senza interpretazioni speciali.
+    // " = In PHP, le virgolette doppie permettono l'interpolazione di variabili e riconoscono le sequenze di escape.
+    // ' = Le virgolette singole (') trattano il contenuto come testo letterale senza interpretazioni speciali.
 
+    // Stampare a video il contenuto della lista 
+        // utilizzando un ciclo for-each --> chiave:valore
+    $lista = array(1,2,3,4,5,6,7,8,9,0);
+ 
+    // for(<tipo> nome : lista <-- Java
+    echo("<h1>Contenuto lista</h1>");
+    foreach($lista as $k=>$v){
+        echo("<p>$k: $v</p>"); // <p></p> per andare a capo
+    }
 ?>
-
