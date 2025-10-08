3ec3353b69f1db05f9646f4678aefcca83336012
Francy <tresoldi.francesco@studenti.salesianisesto.it>
2025-10-08 13:27:28 +0200

    Update primo.php

--- WebServer/Teoria/EsercitazionePHP/primo.php
+++ WebServer/Teoria/EsercitazionePHP/primo.php
@@ -1,9 +1,11 @@
 <?php
 // Il codice php
-	$nome = "Paolo"; // Variabile che contiene il nome
-	$l = '<h1 style="color: red;">Hello $nome</h1>';
-	echo($1);
+	$nome = "Francy"; // Variabile che contiene il nome
+	$l = "<h1 style='color: red;'>Hello $nome</h1>";
+	echo($l);
+
+// " = In PHP, le virgolette doppie permettono l'interpolazione di variabili e riconoscono le sequenze di escape.
+// ' = Le virgolette singole (') trattano il contenuto come testo letterale senza interpretazioni speciali.
+
 ?>
 
-" = intrepretato da php
-' = no
