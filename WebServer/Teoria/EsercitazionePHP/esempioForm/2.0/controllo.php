1c969f725563a466b01fabf10892399f88657814
Francy <tresoldi.francesco@studenti.salesianisesto.it>
2025-10-13 13:38:28 +0200

    Update controllo.php

--- WebServer/Teoria/EsercitazionePHP/esempioForm/2.0/controllo.php
+++ WebServer/Teoria/EsercitazionePHP/esempioForm/2.0/controllo.php
@@ -1,1 +1,13 @@
+<?php
+    $nomeUtente = $_GET["nomeUtente"];
+    $PSW = $_GET["PSW"];
+?>
 
+<head>
+    <title>Benvenuto</title>
+    </head>
+    <body>
+        <h1>Benvenuto</h1>
+        <?php echo("<p>Nome Utente: $nomeUtente Password: $PSW</p>");?>
+    </body>
+</html>
