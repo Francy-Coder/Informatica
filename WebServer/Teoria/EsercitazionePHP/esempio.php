62c5d1fe619f2ac837423883bcb8f78797d411de
Francy <tresoldi.francesco@studenti.salesianisesto.it>
2025-10-13 13:03:09 +0200

    Delete WebServer/Teoria/EsercitazionePHP/esempio.php

--- WebServer/Teoria/EsercitazionePHP/esempio.php
+++
@@ -1,9 +0,0 @@
-<?php $nome = "paolo";?>
-<html>
-    <head></head>
-    <body>
-        <h1>PRIMA WEBAPP</h1> 
-        <p>Ciao <?php echo($nome);?></p>
-        <?php echo("<p>tipo: ".gettype($nome)."</p><p>".var_dump($nome)."</p>");?>
-    </body>
-</html>
