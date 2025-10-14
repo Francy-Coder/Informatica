2eebb5c39e2a0ff0ef972729c7bf568dad34e888
Francy <tresoldi.francesco@studenti.salesianisesto.it>
2025-10-14 14:30:19 +0200

    Update controllo.php

--- WebServer/Teoria/EsercitazionePHP/esempioForm/2.0/controllo.php
+++ WebServer/Teoria/EsercitazionePHP/esempioForm/2.0/controllo.php
@@ -1,13 +1,27 @@
 <?php
-    $nomeUtente = $_GET["nomeUtente"];
-    $PSW = $_GET["PSW"];
+$utentiValidi = array("admin" => "admin123", "mario" => "rossi2024", "utente1" => "password1", "guest" => "guest");
+$nomeUtente = $_GET["nomeUtente"];
+$PSW = $_GET["PSW"];
 ?>
 
+<!DOCTYPE html>
+<html>
 <head>
-    <title>Benvenuto</title>
-    </head>
-    <body>
-        <h1>Benvenuto</h1>
-        <?php echo("<p>Nome Utente: $nomeUtente Password: $PSW</p>");?>
-    </body>
+    <title>Controllo Login</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Risultato Login</h1>
+    <?php
+    if (array_key_exists($nomeUtente, $utentiValidi) && $utentiValidi[$nomeUtente] == $PSW) {
+        echo "<p style='color: green;'><strong>Utente Loggato</strong></p>";
+        echo "<p>Benvenuto, $nomeUtente!</p>";
+    } else {
+        echo "<p style='color: red;'><strong>Credenziali Errate</strong></p>";
+        echo "<p>Login o password non corretti.</p>";
+    }
+    ?>
+    <br>
+    <a href="index.html">Torna al login</a>
+</body>
 </html>
