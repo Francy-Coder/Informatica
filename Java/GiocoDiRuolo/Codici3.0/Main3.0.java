3882c3dab22f7e6fcc1c8c8f3a305d03538d6671
Francy <tresoldi.francesco@studenti.salesianisesto.it>
2025-10-16 13:21:38 +0200

    Fix output message formatting in Main3.0.java

--- Java/GiocoDiRuolo/Codici3.0/Main3.0.java
+++ Java/GiocoDiRuolo/Codici3.0/Main3.0.java
@@ -133,7 +133,7 @@ public class Main {
 
                 switch (sceltaMenu) {
                     case 1:
-                        System.out.println("Per ora vuoto... Prova a inserire 3.. " + /n + "Autore: Francy-Coder | Data: 2024-2025");
+                        System.out.println("Per ora vuoto... Prova a inserire 3.. \nAutore del codice: Francy-Coder");
                         //Iniziare a fare tutte le casistiche con switch e if (es: se vuoi andare avanti o indietro di stanza, o se attaccare o usare una pozione o se scappare) dopo aver fatto:
                             //Zaino 🔎
                             //Metodi Attacca e Subisci Danno 🔎
