28022865c43bea1730b96c1a37c8b744482dc43d
Francy <187552164+Francy-Coder@users.noreply.github.com>
2025-11-23 22:09:59 +0100

    Update index.php

--- WebServer/Esercizi/GestioneCarelloJSON/index.php
+++ WebServer/Esercizi/GestioneCarelloJSON/index.php
@@ -1,47 +1,59 @@
+<?php
+session_start();
+session_set_cookie_params(3600);
+
+if (isset($_SESSION["utente"])) {
+    header("Location: oggetti.php");
+    exit;
+}
+?>
+
 <html>
-    <head></head>
-    <body>
-        <h1>Login Gestione Carrello</h1>
-        <form action="index.php" method="POST">
-            Nome: <input type="text" name="nome"><br>
-            Cognome: <input type="text" name="cognome"><br>
-            ID: <input type="number" name="id"><br>
-            <input type="submit" value="Accedi">
-        </form>
-    </body>
+<head><title>Login</title></head>
+<body>
+<h1>Login Gestione Carrello</h1>
+
+<form action="index.php" method="POST">
+    Nome: <input type="text" name="nome" required><br>
+    Cognome: <input type="text" name="cognome" required><br>
+    ID: <input type="number" name="id" required><br>
+    <input type="submit" value="Accedi">
+</form>
+</body>
 </html>
 
 <?php
-$nome = $_POST["nome"];
-$cognome = $_POST["cognome"];
-$ID = $_POST["id"];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome = $_POST["nome"];
+    $cognome = $_POST["cognome"];
+    $id = $_POST["id"];
 
-$nomeFile = "utente.json";
+    $fileUtenti = "utente.json";
 
-if (!file_exists($nomeFile)) {
-    die("Errore di Sistema");
-}
-
-$json = file_get_contents($nomeFile);
-$arrayUtenti = json_decode($json, true);
-
-$utenteTrovato = false;
-
-foreach ($arrayUtenti as $utente) {
-    if ($utente['nome'] === $nome && $utente['cognome'] === $cognome && $utente['id'] == $ID) {
-        $utenteTrovato = true;
-        break;
+    if (!file_exists($fileUtenti)) {
+        die("Errore di sistema: utenti non trovati.");
     }
-}
 
-if ($utenteTrovato) {
-    echo "<p style='color: green;'>Credenziali corrette</p>";
+    $utenti = json_decode(file_get_contents($fileUtenti), true);
 
-    include("mostraCarello.php");
-    include("oggetti.php");
-    mostraCarello();
+    $trovato = false;
+    foreach ($utenti as $utente) {
+        if ($utente["nome"] === $nome &&
+            $utente["cognome"] === $cognome &&
+            $utente["id"] == $id) {
+            
+            $trovato = true;
+            $_SESSION["utente"] = $utente;
 
-} else {
-    echo "<p style='color: red;'>Credenziali errate</p>";
+            if (!isset($_SESSION["carrello"])) {
+                $_SESSION["carrello"] = [];
+            }
+
+            header("Location: oggetti.php");
+            exit;
+        }
+    }
+
+    echo "<p style='color:red;'>Credenziali errate</p>";
 }
 ?>
