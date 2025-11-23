048a359b00072ed86cba97dc004ed5403fb54cc4
Francy <187552164+Francy-Coder@users.noreply.github.com>
2025-11-23 22:10:29 +0100

    Update oggetti.php

--- WebServer/Esercizi/GestioneCarelloJSON/oggetti.php
+++ WebServer/Esercizi/GestioneCarelloJSON/oggetti.php
@@ -1,4 +1,43 @@
 <?php
-//Session & Server & cookie & expldoe
+session_start();
 
+if (!isset($_SESSION["utente"])) {
+    header("Location: index.php");
+    exit;
+}
+
+$listaOggetti = json_decode(file_get_contents("oggetti.json"), true);
+
+if (isset($_GET["add"])) {
+    $id = $_GET["add"];
+
+    if (!isset($_SESSION["carrello"][$id])) {
+        $_SESSION["carrello"][$id] = 1;
+    } else {
+        $_SESSION["carrello"][$id]++;
+    }
+}
 ?>
+
+<html>
+<head><title>Lista Oggetti</title></head>
+<body>
+
+<h1>Benvenuto <?= $_SESSION["utente"]["nome"]; ?></h1>
+
+<a href="mostraCarrello.php">Mostra Carrello</a>
+<hr>
+
+<h2>Lista Oggetti</h2>
+
+<ul>
+<?php foreach ($listaOggetti as $obj): ?>
+    <li>
+        <?= $obj["nome"]; ?>  
+        <a href="oggetti.php?add=<?= $obj["id"]; ?>">[Aggiungi al carrello]</a>
+    </li>
+<?php endforeach; ?>
+</ul>
+
+</body>
+</html>
