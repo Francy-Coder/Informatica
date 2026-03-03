8c24406c6091d4db6a12057ad69bede2fb85062d
Francy <187552164+Francy-Coder@users.noreply.github.com>
2026-03-03 09:50:25 +0100

    Add query for user age statistics with loans

--- MySQL/Esercizi/EsercitazioneGROUP-BY&Aggregazioni/EsercitazioneGROUP-BY&Aggregazioni.sql
+++ MySQL/Esercizi/EsercitazioneGROUP-BY&Aggregazioni/EsercitazioneGROUP-BY&Aggregazioni.sql
@@ -59,6 +59,4 @@ SELECT Libri.titolo, Libri.autore, COUNT(*) AS numero_prestiti_libri FROM Biblio
 SELECT AVG(eta) AS eta_media FROM Biblioteca.Utenti;
 SELECT Utenti.eta, COUNT(*) AS numero_utenti FROM Biblioteca.Utenti GROUP BY Utenti.eta;
 SELECT Libri.autore, COUNT(*) AS numero_prestiti FROM Biblioteca.Libri INNER JOIN Biblioteca.Prestiti ON Libri.id_libro = Prestiti.id_libro GROUP BY Libri.autore;
-
--- eta minima, massima e media degli utenti che hanno effettuato alemeno un prestito
 SELECT MIN(Utenti.eta) AS eta_minima, AVG(Utenti.eta) AS eta_media, MAX(Utenti.eta) AS eta_massima FROM Biblioteca.Utenti INNER JOIN Biblioteca.Prestiti ON Utenti.id_utente = Prestiti.id_utente;
