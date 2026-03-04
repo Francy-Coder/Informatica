8bcb4afacc41b24da89b6ac890c6c30a6cab0923
Francy <187552164+Francy-Coder@users.noreply.github.com>
2026-03-04 13:38:58 +0100

    Create index.php

---
+++ MySQL/Esercizi/EsercitazionePHP&SQL/index.php
@@ -0,0 +1,39 @@
+<html>
+<head></head>
+<body>
+    <h1>Esercitazione PHP e SQL</h1>
+    <p>Cerca persone tramite l'età</p>
+    <form method="POST">
+        Età: <input type="number" name="eta" required>
+        <input type="submit" value="Invia">
+    </form>
+</body>
+</html>
+
+<?php
+$eta = $_POST["eta"]; // prende il valore se inviato, altrimenti null
+
+// Connessione al database
+$conn = new mysqli("127.0.0.1", "root", "", "Biblioteca");
+
+if($conn->connect_error){
+    die("Connessione fallita: " . $conn->connect_error);
+}
+
+// Esegui la query solo se $eta è valorizzata e numerica
+    $sql = "SELECT * FROM Utenti WHERE eta = $eta";
+    $risultato = $conn->query($sql);
+
+    if($risultato->num_rows > 0){
+        while($riga = $risultato->fetch_assoc()){
+            echo "ID_Utente: " . $riga["id_utente"] . "<br>";
+            echo "Nome: " . $riga["nome"] . "<br>";
+            echo "Cognome: " . $riga["cognome"] . "<br>";
+            echo "Email: " . $riga["email"] . "<br>";
+            echo "Età: " . $riga["eta"] . "<br><hr>";
+        }
+    } else {
+        echo "Nessun utente trovato con età $eta.";
+    }
+$conn->close();
+?>
